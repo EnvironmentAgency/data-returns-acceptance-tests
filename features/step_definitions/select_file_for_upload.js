04c6b943cc019e6e9b5b20ad2b6484836819cb40
timstone <tim.stone@yahoo.co.uk>
2016-08-09 15:35:43 +0100

    fixed file upload

--- features/step_definitions/select_file_for_upload.js
+++ features/step_definitions/select_file_for_upload.js
@@ -1,47 +1,40 @@
 module.exports = function() {
+  var fileChooserXpath = "//input[@type='file']";
 
   this.Given(/^I choose date file (.*)$/, function(filename) {
-    browser.chooseFile("#file-select-button", `features/support/files/Date_format/${filename}`);
+    browser.chooseFile(fileChooserXpath, `features/support/files/Date_format/${filename}`);
   });
 
   this.Given(/^I choose header file (.*) to upload$/, function(filename) {
-    browser.chooseFile("#file-select-button", `features/support/files/Header_format/${filename}`);
+    browser.chooseFile(fileChooserXpath, `features/support/files/Header_format/${filename}`);
   });
 
   this.Then(/^I choose initial file "([^"]*)" to upload$/, function(filename) {
-    browser.chooseFile("#file-select-button", `features/support/files/Initial_Validation/${filename}`);
+    browser.chooseFile(fileChooserXpath, `features/support/files/Initial_Validation/${filename}`);
   });
 
-  //this.Given(/^I choose file "([^"]*)"$/, function(filename) {
-    //browser.chooseFile("#file-select-button", `features/support/files/File_Validation/${filename}`);
-  //});
-
-  //this.Given(/^I choose multiple file (.*)$/, function(filename) {
-    //browser.chooseFile("#file-select-button", `features/support/files/Initial_Validation/${filename}`);
-  //});
-
   this.Given(/^I choose multiple files "([^"]*)"$/, function(filename) {
-    browser.chooseFile("#file-select-button", `features/support/files/Initial_Validation/${filename}`);
+    browser.chooseFile(fileChooserXpath, `features/support/files/Initial_Validation/${filename}`);
   });
 
   this.Given(/^I choose Controlled List file (.*) to upload$/, function(filename) {
-    browser.chooseFile("#file-select-button", `features/support/files/Initial_Validation/${filename}`);
+    browser.chooseFile(fileChooserXpath, `features/support/files/Initial_Validation/${filename}`);
   });
 
   this.Given(/^I choose successfull file (.*) to upload$/, function(filename) {
-    browser.chooseFile("#file-select-button", `features/support/files/Initial_Validation/${filename}`);
+    browser.chooseFile(fileChooserXpath, `features/support/files/File_Validation/${filename}`);
   });
 
   this.Given(/^I choose failed file "([^"]*)" to upload$/, function(filename) {
-    browser.chooseFile("#file-select-button", `features/support/files/File_Validation/${filename}`);
+    browser.chooseFile(fileChooserXpath, `features/support/files/File_Validation/${filename}`);
   });
 
   this.Given(/^I choose boolean file (.*) to upload$/, function(filename) {
-    browser.chooseFile("#file-select-button", `features/support/files/Boolean_values/${filename}`);
+    browser.chooseFile(fileChooserXpath, `features/support/files/Boolean_values/${filename}`);
   });
 
   this.Given(/^I choose file (.*)$/, function(filename) {
-    browser.chooseFile("#file-select-button", `features/support/files/File_Validation/${filename}`);
+    browser.chooseFile(fileChooserXpath, `features/support/files/File_Validation/${filename}`);
   });
 
 }
