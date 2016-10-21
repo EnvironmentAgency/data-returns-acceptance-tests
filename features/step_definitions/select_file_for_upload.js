4341254422d3fe8edb49c7949b64ef0d12562227
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2016-10-21 15:23:57 +0100

    Fixed random upload failures found when using firefox to upload files

--- features/step_definitions/select_file_for_upload.js
+++ features/step_definitions/select_file_for_upload.js
@@ -1,7 +1,10 @@
-module.exports = function() {
-  var fileChooserXpath = "//input[@type='file']";
+module.exports = function () {
+    var fileChooserXpath = "//input[@type='file']";
 
-  this.Then(/^I choose file (.*) to upload$/, function(filename) {
-    browser.chooseFile(fileChooserXpath, `features/support/files/${filename}`);
-  });
+    this.Then(/^I choose file (.*) to upload$/, function (filename) {
+        // Wait for the uploader to become present and upload a file (without waiting we get random errors when using firefox)
+        let uploader = browser.element(fileChooserXpath);
+        uploader.waitForExist(5000);
+        browser.chooseFile(fileChooserXpath, `features/support/files/${filename}`);
+    });
 };
