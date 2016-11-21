7974859b355ebd91d77680397db0be7629ff4dc7
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2016-11-21 18:35:29 +0000

    Add sanity check to test selenium integration with the browser
    - Check local page load to test if proxy settings are causing failures.

---
+++ features/step_definitions/events.js
@@ -0,0 +1,27 @@
+'use strict';
+var handlers = function () {
+    this.registerHandler('BeforeFeatures', function (features, callback) {
+        let file = __dirname + "/sanity_test.html";
+
+        browser.url("file://" + file);
+        let testDiv = browser.element("div#sanity-test");
+        testDiv.waitForExist(5000);
+        if (testDiv.getText() !== "OK") {
+            throw new Error("Sanity test failed.  Browser integration not working correctly.");
+        }
+        console.log("Sanity test passed.\r\n")
+
+        console.log("Features to run: ");
+        for (let feature of features) {
+            console.log("  - " + feature.getName());
+        }
+        console.log("\r\n\r\n");
+
+        callback();
+    });
+    this.registerHandler('AfterFeatures', function (features, callback) {
+        callback();
+    });
+}
+
+module.exports = handlers;
