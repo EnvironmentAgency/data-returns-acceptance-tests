91344acda5e99b82e5f7f6adaf6bba04f87264fe
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2016-11-22 15:49:58 +0000

    Allow sanity test to be skipped via env var (SANITY_TEST=false)

--- features/step_definitions/events.js
+++ features/step_definitions/events.js
@@ -1,16 +1,17 @@
 'use strict';
 var handlers = function () {
     this.registerHandler('BeforeFeatures', function (features, callback) {
-        let file = __dirname + "/sanity_test.html";
-
-        browser.url("file://" + file);
-        let testDiv = browser.element("div#sanity-test");
-        testDiv.waitForExist(5000);
-        if (testDiv.getText() !== "OK") {
-            throw new Error("Sanity test failed.  Browser integration not working correctly.");
+        if (process.env.SANITY_TEST !== 'false') {
+            console.log("Running sanity test");
+            let file = __dirname + "/sanity_test.html";
+            browser.url("file://" + file);
+            let testDiv = browser.element("div#sanity-test");
+            testDiv.waitForExist(5000);
+            if (testDiv.getText() !== "OK") {
+                throw new Error("Sanity test failed.  Browser integration not working correctly.");
+            }
+            console.log("Sanity test passed.\r\n")
         }
-        console.log("Sanity test passed.\r\n")
-
         console.log("Features to run: ");
         for (let feature of features) {
             console.log("  - " + feature.getName());
