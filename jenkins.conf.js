7638cf2da618087a046f1efe85af79c49a1ead70
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2016-12-12 10:50:51 +0000

    Integrated firefox service profile to allow us to set browser proxy settings.

--- jenkins.conf.js
+++ jenkins.conf.js
@@ -13,11 +13,11 @@ const setupCapabilities = function (capabilitiesArray) {
         maxInstances: 1
     };
 
-    if (process.env.BROWSER_PROXY) {
-        common.proxyType = 'manual';
-        common.httpProxy = process.env.BROWSER_PROXY;
-        common.sslProxy = process.env.BROWSER_PROXY;
-    }
+    // if (process.env.BROWSER_PROXY) {
+    //     common.proxyType = 'manual';
+    //     common.httpProxy = process.env.BROWSER_PROXY;
+    //     common.sslProxy = process.env.BROWSER_PROXY;
+    // }
 
     return capabilitiesArray.map(cap => lodash.merge({}, cap, common));
 };
@@ -52,8 +52,17 @@ let localConfig = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['selenium-standalone'],
+    services: ['selenium-standalone', 'firefox-profile'],
+
     seleniumLogs: './logs/selenium',
+
+    firefoxProfile: {
+        'network.proxy.type': 1,
+        'network.proxy.http': process.env.BROWSER_PROXY_HOST,
+        'network.proxy.http_port': parseInt(process.env.BROWSER_PROXY_PORT) || 3128,
+        'network.proxy.ssl': process.env.BROWSER_PROXY_HOST,
+        'network.proxy.ssl_port': parseInt(process.env.BROWSER_PROXY_PORT) || 3128,
+    },
 
     // Disabled screenshots as these are failing when using the firefox driver
     screenshotOnReject: false,
