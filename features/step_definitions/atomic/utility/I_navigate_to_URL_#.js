ccb10a5b334680cc4fa142dca1ab6bae32fdeb1f
graham-willis-druid <graham.willis.ea@gmail.com>
2017-03-21 15:40:21 +0000

    Remove unused

--- features/step_definitions/atomic/utility/I_navigate_to_URL_#.js
+++ features/step_definitions/atomic/utility/I_navigate_to_URL_#.js
@@ -1,5 +1,4 @@
 'use strict';
-const waitForNavigation = require("../../../support/lib/wait-for-navigation-on-action");
 module.exports = function () {
     this.defineStep(/^I navigate to URL "([^"]*)"$/, function (url) {
         browser.url(url);
